2b2f6f9b53dde1363fe42fbc3c48f4afda3dc601
Malhar Deshmukh <malhar_deshmukh@outlook.com>
2016-12-27 18:47:29 +0530

    first commit

---
+++ PROJECT.ino
@@ -0,0 +1,24 @@
+#define DEBUG
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(250000);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValueA = digitalRead(A0);
+  //int sensorValueB = digitalRead(A1);
+#ifdef DEBUG
+  Serial.println(sensorValueA);
+  //Serial.println(sensorValueB);
+#endif
+  delay(1);   
+  
+
+
+
+}
